b62020eab5be20f7ae2bd0375a37d596d9b980d7
Anthematics <Jasonharder2@gmail.com>
2018-10-27 00:37:07 -0400

    Business information is now in App.js & Information is now being passed from app.js to Business / BusinessList child components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import SearchBar from './Components/SearchBar/SearchBar.js'
 import BusinessList from './Components/BusinessList/BusinessList.js'
 import Business from './Components/Business/Business.js'
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [Business,Business,Business,Business,Business,Business]
+//Maybe not in capitals ^^
 class App extends Component {
   render() {
     return (
-      <div class="App">
+      <div className="App">
       <h1>Ravenous</h1>
     <SearchBar />
-    <BusinessList />
+    <BusinessList businesses = {businesses}/>
     </div>
     );
   }
