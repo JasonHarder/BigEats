e7de55b083ffc74627390c583b214b2a32bdb078
Anthematics <Jasonharder2@gmail.com>
2018-11-08 19:40:55 -0500

    added handleSortByChange function that uopdates the state and makes it = to the css class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,8 +15,22 @@ function getSortbyClass (sortByOption) {
     }
 
 }
-// ^^^ getSortByClass would not return anything if I did not refer to this.state
-// since it is in the this.state object
+
+/*
+
+1. getSortByClass would not return anything if I did not
+refer to this.state since it is in the this.state object
+
+2. This Function Returns the current CSS class for a sorting option.
+*/
+
+function handleSortByChange(sortByOption) {
+    this.setState(this.sortBy = sortByOption)
+}
+/*
+This second method (handleSortByChange) updates the state by calling .setState to set the state to the sortByOption
+this seems to mean that the css class should === the state.
+*/
 class Searchbar extends React.Component {
     constructor(props) {
         super(props)
