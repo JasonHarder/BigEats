26d169634242865c5a27dbfec74f50dfd7655867
Anthematics <Jasonharder2@gmail.com>
2018-11-22 17:06:25 -0500

    working on beginnings of Yelp Search API function

---
+++ src/util/yelp.js
@@ -0,0 +1,20 @@
+const apiKey = 'for british eyes only'
+
+const Yelp = {}
+
+const search = (term,location,sortBy) => {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {headers:{
+        Authorization: `Bearer ${apiKey}`}
+      }
+  ).then(response => {
+    return response.json()
+  }).then(jsonResponse => {
+    if (jsonResponse.businesses) {
+      return jsonResponse.businesses.map(jsonBusiness =>  )
+    }
+  });
+}
+
+export default Yelp;
