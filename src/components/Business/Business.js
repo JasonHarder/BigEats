d534c100decfc177b288a75d386dc0f54d00529c
Anthematics <Jasonharder2@gmail.com>
2018-11-29 18:30:48 -0500

    object returns are uneven , make yelp js async?

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -16,16 +16,16 @@ class Business extends React.Component {
             <h2>{this.props.business.name}</h2>
             <div className="Business-information">
                 <div className="Business-address">
-                    <p> {this.props.business.address} <br/>
-                        {this.props.business.city}  <br/>
-                        {this.props.business.state} <br/>
-                        {this.props.business.zipCode}
+                    <p> street address:{this.props.business.address}
+                        city: {this.props.business.city}
+                        state:{this.props.business.state}
+                        zip/postal code{this.props.business.zipCode}
                     </p>
                 </div>
                 <div className="Business-reviews">
                 <h3>{this.props.business.category}</h3>
-                <h3 className="rating">{this.props.business.rating}</h3>
-                <p>{this.props.business.reviewCount}</p>
+                <h2 className="rating">Rating {this.props.business.rating}</h2>
+                <p>Review Count {this.props.business.reviewCount}</p>
                 </div>
             </div>
             </div>
