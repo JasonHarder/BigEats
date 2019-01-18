7b6bf353802202f6fe70824f683721945225cbc1
Anthematics <Jasonharder2@gmail.com>
2019-01-18 03:15:51 -0500

    gitignored src public build/static css & buildstatic js

---
+++ build/precache-manifest.cff218f46090b4d0963f124c56052824.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Anthematics/BigEats/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Anthematics/BigEats/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Anthematics/BigEats/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "f42078fd98b86e773a98",
+    "url": "/Anthematics/BigEats/static/js/runtime~main.f42078fd.js"
+  },
+  {
+    "revision": "dfb4268b21476de961b3",
+    "url": "/Anthematics/BigEats/static/js/main.dfb4268b.chunk.js"
+  },
+  {
+    "revision": "e895df9760dadda52d77",
+    "url": "/Anthematics/BigEats/static/js/1.e895df97.chunk.js"
+  },
+  {
+    "revision": "dfb4268b21476de961b3",
+    "url": "/Anthematics/BigEats/static/css/main.c802cbf1.chunk.css"
+  },
+  {
+    "revision": "918163f300bcda013b7b05cdf18fe662",
+    "url": "/Anthematics/BigEats/index.html"
+  }
+];
